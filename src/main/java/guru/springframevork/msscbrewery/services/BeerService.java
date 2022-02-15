af1a64c375bc92f524edddc414dd77b7e377a6ee
Amuhadri <muhadri.amir@gmail.com>
2022-02-15 14:16:18 +0100

    http get with Spring MVC

---
+++ src/main/java/guru/springframevork/msscbrewery/services/BeerService.java
@@ -0,0 +1,12 @@
+package guru.springframevork.msscbrewery.services;
+
+import java.util.UUID;
+
+import guru.springframevork.msscbrewery.web.model.BeerDto;
+
+public interface BeerService {
+
+	BeerDto getBeerById(UUID beerId);
+	
+
+}
